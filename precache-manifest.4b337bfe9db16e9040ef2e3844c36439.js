16a7986fdccddb71e3cc8558a648f53f6dbddcc2
Nikash Taskar <nikashtaskar@wustl.edu>
2019-04-11 23:34:20 -0500

    Updates

---
+++ precache-manifest.4b337bfe9db16e9040ef2e3844c36439.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7feb9c688c8a605dd933",
+    "url": "/09-todo-react-niktaskar/static/js/runtime~main.7feb9c68.js"
+  },
+  {
+    "revision": "971b415fdfb571cd139b",
+    "url": "/09-todo-react-niktaskar/static/js/main.971b415f.chunk.js"
+  },
+  {
+    "revision": "10782fe46602e17ec45b",
+    "url": "/09-todo-react-niktaskar/static/js/1.10782fe4.chunk.js"
+  },
+  {
+    "revision": "971b415fdfb571cd139b",
+    "url": "/09-todo-react-niktaskar/static/css/main.af88ea0d.chunk.css"
+  },
+  {
+    "revision": "fef77b80d639ab8e00ce147a6f7a9010",
+    "url": "/09-todo-react-niktaskar/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/todo-react/precache-manifest.857ce18bebcba928b3be6d240402ba41.js"
+  "/09-todo-react-niktaskar/precache-manifest.4b337bfe9db16e9040ef2e3844c36439.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/todo-react/index.html", {
+workbox.routing.registerNavigationRoute("/09-todo-react-niktaskar/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
